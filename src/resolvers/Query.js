12e65e1a0e0daa826c1cc362ba287cf7a0af773e
neword <newordn@gmail.com>
2020-01-09 18:56:49 +0100

    Added cowsByHerd Mutation

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,13 +4,24 @@ const users = async (parent,args,context,info)=>{
         status:true}})
     return users
 }
+// to list of the available herds
 const herds = async (parent,args,context,info)=>{
     const herds = await context.prisma.herds({orderBy: 'id_DESC',where:{
         status:true}})
     return herds
 }
+// to list of the available cows a herd
+const cowsByHerd = async (parent,args,context,info)=>{
+    const cows = await context.prisma.herd({id:args.herdId}).cows({
+        orderBy:'id_DESC',where:{
+            status:true
+        }
+    })
+    return cows
+}
 module.exports={
     info,
     users,
-    herds
+    herds,
+    cowsByHerd
 }
