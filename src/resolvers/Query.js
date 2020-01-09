eea525a2c4157480047c29796c5ceab52dab87b1
neword <newordn@gmail.com>
2020-01-09 11:02:25 +0100

    Added users Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,9 @@
 const info = ()=>`Welcome to Mokine's graphql server`
-
+const users = async (parent,args,context,info)=>{
+    const users = await context.prisma.users({orderBy: 'id_DESC'})
+    return users
+}
 module.exports={
-    info
+    info,
+    users
 }
