3f155521cc5df71933fa4da81f5491e24d694e7d
neword <newordn@gmail.com>
2020-01-09 10:57:09 +0100

    Added SignIn mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,11 +3,28 @@ const {APP_SECRET} = require('../../helpers/user')
 const jwt = require('jsonwebtoken')
 const signUp = async (parent,args,context,info)=>{
     const password = await bcrypt.hash(args.password,10)
-    const user = await context.prisma.createUser({...args})
+    const user = await context.prisma.createUser({...args,password})
     const token = jwt.sign({userId:user.id},APP_SECRET)
     return {token,user}
 }
+async function signIn(parent,args,context,info)
+{
+    const user =  await context.prisma.user({phone:args.phone})
+    if(!user){
+        throw new Error("L'utilisateur n'existe pas. Inscrivez-vous")
+    }
+    const valid = await bcrypt.compare(args.password,user.password)
+    if(!valid){
+        throw new Error('Mot de passe incorrect')
+    }
+    const token = jwt.sign({userId: user.id},APP_SECRET)
+    return {
+        token,
+        user
+    }
+}
 
 module.exports={
-    signUp
+    signUp,
+    signIn
 }
