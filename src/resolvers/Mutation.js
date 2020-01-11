8f0684dd0f82555cec6b1997dc780ecc8986785c
neword <newordn@gmail.com>
2020-01-11 23:22:21 +0100

    update

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -94,7 +94,7 @@ const cow = async (parent,args,context,info)=>{
     console.log("Create a cow mutation")
     const images = await Promise.allSettled(args.images.map(async v=>await context.storeUpload(v)))
     const cow  = context.prisma.createCow({...args,status:true,images:{set:images},herd:{connect:{id:args.herd}}})
-    return user
+    return cow
     }
            // to create a herd
 const herd = async (parent,args,context,info)=>{
