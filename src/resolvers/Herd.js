7a16209d92ab5238b0b84324c918c0dab1d210a6
neword <newordn@gmail.com>
2020-01-11 21:42:25 +0100

    old commit

--- src/resolvers/Herd.js
+++ src/resolvers/Herd.js
@@ -1,7 +1,10 @@
 const catterys= (parent,args,context,info)=>{
     return context.prisma.herd({id:parent.id}).catterys()
 }
-
+const cows= (parent,args,context,info)=>{
+    return context.prisma.herd({id:parent.id}).cows()
+}
 module.exports={
-    catterys
+    catterys,
+    cows
 }
