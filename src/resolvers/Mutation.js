0557057731e26f15ce796bb2da1adfd91d41736a
neword <newordn@gmail.com>
2020-01-16 14:35:27 +0100

    old commit

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -94,7 +94,8 @@ const cow = async (parent,args,context,info)=>{
     console.log("Create a cow mutation")
     let images = await Promise.all(args.images.map(async v=>await context.storeUpload(v)))
     images = images.map(v=>v.path)
-    const myId = new Date().getTime()
+    const myId = new Date().getTime().toString()
+    console.log(myId)
     const cow  = await context.prisma.createCow({...args,status:true,images:{set:images},herd:{connect:{id:args.herd}},myId })
     return cow
     }
